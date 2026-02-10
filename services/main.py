8fa33b62e0847b326fa538af20242b7b1ac2fe0c
Swanand Mehta <swanandmehta666@gmail.com>
2026-02-10 23:43:22 +0530

    check market service sse

--- services/main.py
+++ services/main.py
@@ -1,6 +1,8 @@
 # services/auth_service/app/main.py
 from fastapi import FastAPI, HTTPException
-from services.angel_auth.app import angel_login
+from services.auth_service.app.angel_auth import angel_login
+from services.market_sse.app.market_sse import ltp_event_generator
+from sse_starlette.sse import EventSourceResponse
 
 app = FastAPI(title="Auth Service")
 
